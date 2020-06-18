b60ea8332e078c34931e802d2292481c0c1a462d
EvaTamm01 <eva.tamm30@gmail.com>
2020-06-18 17:47:37 +0300

    menu, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ItemsListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
